5fd021ed3776fc9e6aa360a254a35fb4dff47c3c
minagerges2310 <menagergea@gmaail.com>
2023-07-10 23:35:54 +0300

    task finished

--- task11/KPD_Test/main.c
+++ task11/KPD_Test/main.c
@@ -7,32 +7,45 @@
 /* HAL */
 #include "KPD_INterface.h"
 #include "LCD_interface.h"
+#include <util/delay.h>
+
+
 
 int main(void)
 {
 	u8 Local_u8Key;
 	DIO_voidInit();
 	LCD_voideInit();
-	u8 password[4] = "1234";
-	u8 password_input[4];
+	LCD_PrintString("enter password");
+	u8 password[4] = {'1','2','3','4'};
+	u8 index = 0;
 
 	while(1)
 	{
+		while(index<4)
+		{
+			KPD_u8GetKeyState(&Local_u8Key);
 
-	   KPD_u8GetKeyState(&Local_u8Key);
+			  if(Local_u8Key != KPD_u8_KEY_NOT_PRESSED){
+           LCD_voideInit();
 
-	   if(Local_u8Key != KPD_u8_KEY_NOT_PRESSED){
+	         LCD_voidSendChar(Local_u8Key);
+	         if (password[index] == Local_u8Key){
+	        	 index++;
+		}
+	         else {break;}
 
-	   LCD_voidSendChar(Local_u8Key);
-	   }
-	   for(int i = 0; i < 3; i++){
-	   		password_input[i] = Local_u8Key;}
+	   }}
 
-	   for(int i = 0; i < 3; i++){
-	   	   if(password[i] = password_input[i]){
-	   		   LCD_PrintString("correct");}
+			  if(index >=4){
 
+				  LCD_PrintString("correct");
+				  break;
 	}
-	return 0;
-}}
+			  else{
+				  LCD_PrintString("incorrect");
+				  break;
+			  }
 
+}
+	return 0;}
