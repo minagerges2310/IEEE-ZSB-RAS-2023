80b45364d94970e33e0385d58ddbca352e8ffb49
minagerges2310 <menagergea@gmaail.com>
2023-08-05 01:52:46 +0300

    draft

---
+++ task16/ADC_test/main.c
@@ -0,0 +1,16 @@
+#include "BIT_Math.h"
+#include "STD_TYPES.h"
+#include "DIO_interface.h"
+#include "ADC_interface.h"
+#include "LCD_interface.h"
+
+
+#include<util/delay.h>
+#define F_CPU 16000000UL
+
+int main(void)
+{
+
+
+	return 0;
+}
